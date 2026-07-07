533f2e625245a2b9bcb8512a724b50f7de393bd7
aloshi2003 <alosiousalbin90@gmail.com>
2026-07-07 15:19:45 +0530

    email updation

--- react-app/src/pages/Leaders.jsx
+++ react-app/src/pages/Leaders.jsx
@@ -167,7 +167,7 @@ const Leaders = () => {
                         <div>
                             <h5 style={{ fontSize: "0.7rem", textTransform: "uppercase", fontWeight: "800", letterSpacing: "0.5px", marginBottom: "0.3rem" }}>STRATEGIC OPERATIONS</h5>
                             <p style={{ fontSize: "0.8rem", color: "#666", margin: "0" }}>Leading the next generation of academic diplomacy.</p>
-                            <p style={{ fontSize: "0.8rem", margin: "0", marginTop: "0.5rem" }}><a href="mailto:abhav@probizindia.in" style={{ color: "var(--color-orange)", textDecoration: "none", fontWeight: "700" }}>abhav@probizindia.in</a></p>
+                            <p style={{ fontSize: "0.8rem", margin: "0", marginTop: "0.5rem", pointerEvents: "auto" }}><a href="mailto:abhav@probizindia.in" style={{ position: "relative", zIndex: "10", color: "var(--color-orange)", textDecoration: "none", fontWeight: "700" }}>abhav@probizindia.in</a></p>
                         </div>
                     </div>
 
@@ -391,6 +391,7 @@ const Leaders = () => {
             letter-spacing: 5px;
             white-space: nowrap;
             z-index: 0;
+            pointer-events: none;
         }
         .vertical-text.left {
             left: -4rem;
@@ -423,6 +424,8 @@ const Leaders = () => {
 
         .leader-content {
             padding: 0 0 2rem;
+            position: relative;
+            z-index: 1;
         }
 
         .accomplishment-item {
